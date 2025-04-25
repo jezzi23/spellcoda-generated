027ede38dc96b8d907bd26bfeadfa6913d36bff2
jezzi23 <jezzi23@gmail.com>
2025-04-25 18:39:55 +0000

    Automated build v0.4.1038

--- vanilla/defs.lua
+++ vanilla/defs.lua
@@ -1,6 +1,6 @@
 -- THIS FILE IS GENERATED
 local _, sc = ...;
-sc.addon_build_id = 1026;
+sc.addon_build_id = 1038;
 sc.client_name_src = "wow_classic_era";
 sc.client_version_src = "1.15.7.60277";
 _, sc.class = UnitClass("player");
