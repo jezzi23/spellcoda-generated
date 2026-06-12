dc00adee379d38ac7813abf628605484f5008eab
jezzi23 <jezzi23@gmail.com>
2026-06-12 00:37:32 +0000

    Automated build v0.10.2420

--- tbc/defs.lua
+++ tbc/defs.lua
@@ -1,8 +1,8 @@
 -- THIS FILE IS GENERATED
 local _, sc = ...;
-sc.addon_build_id = 2383;
+sc.addon_build_id = 2420;
 sc.client_name_src = "wow_anniversary";
-sc.client_version_src = "2.5.5.67852";
+sc.client_version_src = "2.5.5.68101";
 _, sc.class = UnitClass("player");
 _, _, sc.race = UnitRace("player");
 sc.faction = UnitFactionGroup("player");
